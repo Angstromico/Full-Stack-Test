bcccbbf5b67aa430016192a0dea63ea8ed3965a6
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-20 18:46:02 -0400

    feat: Implement jest test on Register authController

---
+++ Back/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePaths: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
+}
--- Back/src/models/User.ts
+++ Back/src/models/User.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose'
-import bcrypt from 'bcryptjs'
+import * as bcrypt from 'bcryptjs'
 
 export interface IUser extends Document {
   name: string
